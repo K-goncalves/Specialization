3a9ceb0ea2ec92fc222288440af5ae5d57276b50
K-goncalves <rochakenny@hotmail.com>
2022-11-01 17:10:56 -0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    //Add todo to list
+    setList([...list, newTodo]);
+
+    //Clear box
+    setInput("");
+  };
+
+  //Check Box
+  //const checkBox =
+
+  //Remove a Todo
+  const deleteTodo = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  };
+
+  return (
+    <div className="todoDiv">
+      <h1>Todo List For Today</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}>Add New Item</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default App;
